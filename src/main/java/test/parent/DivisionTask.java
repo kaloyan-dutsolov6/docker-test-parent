179333357655968e59f8d4f5982fb7cb8e8a6f43
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-01-19 15:29:19 +0200

    Update DivisionTask.java

--- src/main/java/test/parent/DivisionTask.java
+++ src/main/java/test/parent/DivisionTask.java
@@ -2,15 +2,6 @@ package test.parent;
 
 public class DivisionTask {
     public static int divide(int x, int y) {
-//                 boolean flag = true;
-//         while(flag){
-//             System.out.println("pet");
-//                         try {
-//                 Thread.sleep(1000);
-//             } catch (InterruptedException e) {
-//                 e.printStackTrace();
-//             }
-//         }
-        return x / y;
+        return x - y;
     }
 }
