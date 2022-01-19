f696a48010d39dfe32214af492c5f3428d6b85c5
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-01-19 15:29:00 +0200

    Update MultiplyTask.java

--- src/main/java/test/parent/MultiplyTask.java
+++ src/main/java/test/parent/MultiplyTask.java
@@ -2,11 +2,6 @@ package test.parent;
 
 public class MultiplyTask {
     public static int multiply(int x, int y) {
-        try {
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         return x - y;
     }
 }
