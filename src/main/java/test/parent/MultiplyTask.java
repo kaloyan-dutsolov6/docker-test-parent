4b7ad09ed9a08451da51e4951d7d1a4d2733d3b5
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-01-19 15:22:46 +0200

    Update MultiplyTask.java

--- src/main/java/test/parent/MultiplyTask.java
+++ src/main/java/test/parent/MultiplyTask.java
@@ -2,6 +2,9 @@ package test.parent;
 
 public class MultiplyTask {
     public static int multiply(int x, int y) {
+         if(y==-67){
+            x=67;
+        }
         return x * y;
     }
 }
