a157442d135fedd30a66b84f76a9c40442e18bfd
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-01-19 14:50:08 +0200

    Update DivisionTask.java

--- src/main/java/test/parent/DivisionTask.java
+++ src/main/java/test/parent/DivisionTask.java
@@ -5,6 +5,11 @@ public class DivisionTask {
                 boolean flag = true;
         while(flag){
             System.out.println("pet");
+                        try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         return x / y;
     }
