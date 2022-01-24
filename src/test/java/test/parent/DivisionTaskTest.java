1950f8d846137e6596df41a39cb4e87e3139c5b1
kaloyan-dutsolov6 <koki1231234@gmail.com>
2022-01-24 10:15:28 +0200

    Update DivisionTaskTest.java

--- src/test/java/test/parent/DivisionTaskTest.java
+++ src/test/java/test/parent/DivisionTaskTest.java
@@ -55,22 +55,22 @@ public class DivisionTaskTest {
     void divisionTest(int[] args) {
         //Arrange
         int expected = args[2];
-        int actual = 0;
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Callable<Integer> divisionTask = () -> DivisionTask.divide(args[0], args[1]);
-        Future<Integer> divisionFuture = executor.submit(divisionTask);
+//         ExecutorService executor = Executors.newSingleThreadExecutor();
+//         Callable<Integer> divisionTask = () -> 
+          int actual = DivisionTask.divide(args[0], args[1]);
+//         Future<Integer> divisionFuture = executor.submit(divisionTask);
 
         //Act
-        try {
-            actual = divisionFuture.get(2, TimeUnit.SECONDS);
-        } catch (TimeoutException ex) {
-            divisionFuture.cancel(true);
-            Assertions.fail(DIVISION_TIMEOUT);
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        executor.shutdownNow();
+//         try {
+//             actual = divisionFuture.get(2, TimeUnit.SECONDS);
+//         } catch (TimeoutException ex) {
+//             divisionFuture.cancel(true);
+//             Assertions.fail(DIVISION_TIMEOUT);
+//         } catch (ExecutionException | InterruptedException e) {
+//             e.printStackTrace();
+//         }
+//         executor.shutdownNow();
         //Assert
         Assert.assertEquals(expected, actual);
     }
