8039ee4150fb46fa308bfa8ab55c0f4c0e997f8c
Kaloyan Dutsolov <kaloyan_dutsolov@epam.com>
2022-01-26 11:16:29 +0200

    get containerid

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -53,7 +53,6 @@ public class TestMain {
         HttpService httpService = new HttpService();
         try {
             String containerId = System.getenv().get("HOSTNAME");//.entrySet().forEach(stringStringEntry -> System.out.println(stringStringEntry.getKey() + " " + stringStringEntry.getValue()));
-            System.out.println(args[0] + " " + args[1] + " " + args[2]);
             httpService.sendTestResult(args[0], CustomTestExtension.pointsSum, fails, containerId);
         } catch (IOException e) {
             e.printStackTrace();
