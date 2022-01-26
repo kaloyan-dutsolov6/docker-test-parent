261bff3fc8e5d052af460f708cedd44b2c4d663b
Kaloyan Dutsolov <kaloyan_dutsolov@epam.com>
2022-01-26 10:29:12 +0200

    check args

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -53,7 +53,7 @@ public class TestMain {
         HttpService httpService = new HttpService();
         try {
             System.getenv().entrySet().forEach(stringStringEntry -> System.out.println(stringStringEntry.getKey() + " " + stringStringEntry.getValue()));
-
+            System.out.println(args[0] + " " + args[1] + " " + args[2]);
             httpService.sendTestResult(args[0], CustomTestExtension.pointsSum, fails);
         } catch (IOException e) {
             e.printStackTrace();
