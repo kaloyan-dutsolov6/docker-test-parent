c1e2006423e54db9c0b8e98d3336dd74ca4e2918
Kaloyan Dutsolov <kaloyan_dutsolov@epam.com>
2022-02-18 16:48:06 +0200

    SandBoxMain

---
+++ src/test/java/SandBoxMain.java
@@ -0,0 +1,40 @@
+import model.FailedTestCase;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
+import service.TestFailService;
+import test.parent.DivisionTask;
+import test.parent.MultiplyTask;
+import test.sandbox.SandboxSecurityPolicy;
+
+import java.security.Policy;
+import java.util.List;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
+
+public class SandBoxMain {
+
+    private static final String SANDBOX_TEST = "test.sandbox";
+
+    private static SummaryGeneratingListener runTests(String testPackage) {
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(selectPackage(testPackage))
+                .build();
+        Launcher launcher = LauncherFactory.create();
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+        return listener;
+    }
+
+    public static void main(String[] args) {
+        TestFailService failService = new TestFailService();
+        SummaryGeneratingListener listener = runTests(SANDBOX_TEST);
+        TestExecutionSummary summary = listener.getSummary();
+        List<FailedTestCase> fails = failService.formatFailures(summary.getFailures());
+        failService.printFails(fails);
+    }
+}
